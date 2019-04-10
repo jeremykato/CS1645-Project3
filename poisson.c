d61a0a9be6d7f2c10bdb6c6468beefae8118fd2c
Jeremy Kato <jeremykato@hotmail.com>
2019-04-10 17:11:20 -0400

    debug fuck you

--- poisson.c
+++ poisson.c
@@ -105,7 +105,7 @@ void* jacobi_iteration(void* v_param) {
             exit(-1);
         }
         //printf("%d Lock released, waiting...\n", t_num);
-        
+        printf("%d says max_change is %2.6f", max_change);
         pthread_barrier_wait(barrier);
     }
 
